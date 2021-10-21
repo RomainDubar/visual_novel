bac846582116118169f54d73dc35acf9afe9d9aa
RomainDubar <romain.dubar@lacatholille.fr>
2021-10-21 19:43:31 +0200

    etienne_tool

---
+++ Visual_novel2/Assets/DialogueSystem/Engine/Data/DSNodeData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DS.Engine.Data {
+    [Serializable]
+    public class DSNodeData {
+        public string Id { get => id; set => id = value; }
+        public string CharacterName { get => characterName; set => characterName = value; }
+        public string Text { get => text; set => text = value; }
+        public List<DSChoiceData> Choices { get => choices; set => choices = value; }
+        public Sprite CharacterSprite { get => characterSprite; set => characterSprite = value; }
+        public Sprite BackgroundSprite { get => backgroundSprite; set => backgroundSprite = value; }
+        public Vector2 Position { get => position; set => position = value; }
+
+        [SerializeField] private string id;
+        [SerializeField] private string characterName, text;
+        [SerializeField] private List<DSChoiceData> choices;
+        [SerializeField] private Sprite characterSprite, backgroundSprite;
+        [SerializeField] private Vector2 position;
+
+        public DSNodeData() {
+            id = Guid.NewGuid().ToString();
+            characterName = "Character";
+            text = "Text...";
+            choices = new List<DSChoiceData>();
+            characterSprite = null;
+            backgroundSprite = null;
+            position = Vector3.zero;
+        }
+
+        public DSNodeData(DSNodeData data) {
+            id = data.Id;
+            characterName = data.CharacterName;
+            text = data.Text;
+            choices = Clone(data.Choices);
+            characterSprite = data.CharacterSprite;
+            backgroundSprite = data.BackgroundSprite;
+            position = new Vector2(data.Position.x, data.Position.y);
+        }
+
+        public DSNodeData(string id, string characterName, string text, List<DSChoiceData> choices, Sprite characterSprite, Sprite backgroundSprite, Vector2 position) {
+            this.id = id;
+            this.characterName = characterName;
+            this.text = text;
+            this.choices = choices;
+            this.characterSprite = characterSprite;
+            this.backgroundSprite = backgroundSprite;
+            this.position = position;
+        }
+
+
+        private List<DSChoiceData> Clone(List<DSChoiceData> choices) {
+            List<DSChoiceData> clone = new List<DSChoiceData>();
+            foreach(DSChoiceData choice in choices) {
+                clone.Add(new DSChoiceData(choice));
+            }
+            return clone;
+        }
+    }
+}
---
+++ Visual_novel2/Assets/DialogueSystem/Engine/DialogManager.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DS.Engine {
+    using Data;
+
+    public class DialogManager : MonoBehaviour {
+        [Header("Container")]
+        [SerializeField] private DSContainerSO container;
+        [Header("Text")]
+#if TMP
+        [SerializeField] private TMPro.TextMeshProUGUI textDialog;
+        [SerializeField] private TMPro.TextMeshProUGUI textCharacterName;
+#else
+    [SerializeField] private Text textDialog;
+    [SerializeField] private Text textCharacterName;
+#endif
+        [Header("Images")]
+        [SerializeField] private Image imageCharacter;
+        [SerializeField] private Image imageBackground;
+        [Header("Buttons")]
+        [SerializeField] private GameObject buttonParent;
+        [SerializeField] private GameObject buttonPrefab;
+
+        private Dictionary<string, DSNodeData> dialogues = new Dictionary<string, DSNodeData>();
+        private DSNodeData currentDialogue;
+
+        private void Awake() {
+            foreach(DSNodeData dialogue in container.Nodes) {
+                dialogues.Add(dialogue.Id, dialogue);
+            }
+            currentDialogue = container.FirstNode;
+        }
+
+        private void Start() {
+            UpdateDialogSequence(currentDialogue);
+        }
+
+        private void UpdateDialogSequence(int index) {
+            if(index >= currentDialogue.Choices.Count) return;
+            UpdateDialogSequence(dialogues[currentDialogue.Choices[index].NextDialogueID]);
+        }
+
+        public void UpdateDialogSequence(DSNodeData dialogue) {
+            UpdateTexts(dialogue);
+            UpdateChoices(dialogue);
+            UpdateSprites(dialogue);
+            currentDialogue = dialogue;
+        }
+
+        private void UpdateTexts(DSNodeData dialogue) {
+            if(!string.IsNullOrEmpty(dialogue.Text)) {
+                textDialog.transform.parent.gameObject.SetActive(true);
+                textDialog.text = dialogue.Text;
+                textCharacterName.transform.parent.gameObject.SetActive(true);
+                textCharacterName.text = dialogue.CharacterName;
+            } else {
+                textDialog.transform.parent.gameObject.SetActive(false);
+                textCharacterName.transform.parent.gameObject.SetActive(false);
+            }
+        }
+
+        private void UpdateChoices(DSNodeData dialogue) {
+            List<DSChoiceData> validChoices = new List<DSChoiceData>();
+            foreach(DSChoiceData choice in dialogue.Choices) {
+                if(!string.IsNullOrEmpty(choice.NextDialogueID)) validChoices.Add(choice);
+            }
+            HandleButtons(validChoices, dialogue.Choices);
+        }
+
+        private void HandleButtons(List<DSChoiceData> validChoices, List<DSChoiceData> allChoices) {
+
+            Button[] children = buttonParent.GetComponentsInChildren<Button>();
+            foreach(Button child in children) GameObject.Destroy(child.gameObject);
+
+            List<(Button button, int index)> buttons = new List<(Button button, int index)>();
+            for(int i = 0; i < validChoices.Count; i++) {
+                int index = allChoices.IndexOf(validChoices[i]);
+                GameObject go = GameObject.Instantiate(buttonPrefab, buttonParent.transform);
+                Button button = go.GetComponent<Button>();
+#if TMP
+                TMPro.TextMeshProUGUI text = go.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+#else
+                Text text = go.GetComponentInChildren<Text>();
+#endif
+                text.text = validChoices[i].Name;
+                buttons.Add((button, index));
+            }
+
+            foreach((Button button, int index) button in buttons) {
+                button.button.onClick.RemoveAllListeners();
+                button.button.onClick.AddListener(() => UpdateDialogSequence(button.index));
+            }
+        }
+
+        private void UpdateSprites(DSNodeData dialogue) {
+            if(dialogue.CharacterSprite != null) imageCharacter.sprite = dialogue.CharacterSprite;
+            if(dialogue.BackgroundSprite != null) imageBackground.sprite = dialogue.BackgroundSprite;
+        }
+    }
+}
